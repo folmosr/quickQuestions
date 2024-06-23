bf9c563feee279925c26d8c2f419ad86905729a3
Fabian Olmos <fabianolmos806@gmail.com>
2024-06-23 21:52:50 +0200

    MongoDb update

--- src/mongo/queries.js
+++ src/mongo/queries.js
@@ -8,14 +8,14 @@ db.users.find({
         { email: 'thompsonboone@zyple.com' },
         { first_name: /^thompsonboone@zyple.com/ig },
         { last_name: /^thompsonboone@zyple.com/ig }],
-    last_connection_date: { $lte: '2024-01-31' }
+    last_connection_date: { $lte: ISODate(getFormattedDate()) }
 });
 
 function getFormattedDate() {
     const today = new Date();
     const yyyy = today.getFullYear();
     let rawmm = today.getMonth() - 5; // Months start at 0!
-    let rawdd = today.getDate();
+    let rawdd = today.getDate() + 1;
 
     let dd, mm;
 
@@ -26,7 +26,52 @@ function getFormattedDate() {
         mm = `${rawmm}`
     };
 
-    const formattedToday = yyyy + '-' + mm + '-' + dd;
+    const formattedToday = yyyy + '-' + mm + '-' + dd + 'T00:00:00';
     return formattedToday;
 }
-/**What should be added to the collection so that the query is not slow? I would create an Index  */
+/**What should be added to the collection so that the query is not slow? R = I would create an Index  */
+
+/**
+ * Exercice: MongoDb aggregate
+ */
+[
+
+    {
+        $match: {
+            roles: "email"
+        }
+    },
+    {
+        $group:
+        {
+            _id: { $first: "$roles" },
+            users: { $push: "$email" }
+        }
+    },
+]
+
+/**
+ * MongoDb update
+ */
+
+// Update document ObjectId("5cd96d3ed5d3e20029627d4a"), modify only last_connection_date with current date
+db.users.updateOne({ _id: ObjectId('5cd96d3ed5d3e20029627d4a') }, {
+    $currentDate: {
+        last_connection_date: true
+    }
+})
+// Update document ObjectId("5cd96d3ed5d3e20029627d4a"), add a role admin
+db.users.updateOne({ _id: ObjectId('5cd96d3ed5d3e20029627d4a') },
+    {
+        $push: {
+            roles: "admin"
+        },
+    })
+// Update document ObjectId("5cd96d3ed5d3e20029627d4a"), modify addresses with zip 75001 and replace city with Paris 1
+db.users.updateOne({ _id: ObjectId('5cd96d3ed5d3e20029627d4a') },
+    {
+        $set: {
+            "address.$.zip": 75001,
+            "address.$.city": "Paris 1"
+        },
+    })
